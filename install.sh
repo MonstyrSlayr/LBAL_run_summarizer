ae9e903a6588d09312e5bc7c891dcc3e34e7f3cf
MonstyrSlayr <eehikioya0@gmail.com>
2025-01-26 16:52:16 -0500

    using powershell

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
-if ( "${{ matrix.os }}" == "macos-latest" ) then
-  pyinstaller --onefile --noconsole --icon "icon.icns" \
-    --distpath dist/ src/lbal_run_summarizer.py \
-    --add-data "img:img" \
-    --add-data "fonts:fonts" \
-    --add-data "symbol_data.json:."
-else
-  pyinstaller --onefile --noconsole --icon "icon.ico" \
-    --distpath dist/ src/lbal_run_summarizer.py \
-    --add-data "img:img" \
-    --add-data "fonts:fonts" \
-    --add-data "symbol_data.json:."
-fi
+pyinstaller --onefile --noconsole --icon "icon.ico" \
+  --distpath dist/ src/lbal_run_summarizer.py \
+  --add-data "img:img" \
+  --add-data "fonts:fonts" \
+  --add-data "symbol_data.json:."
